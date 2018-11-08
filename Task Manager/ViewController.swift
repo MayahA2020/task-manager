93a5db73c9fd60597ea8365082f54ad9399a9dba
Kaj-Desch <kmdesch@icloud.com>
2018-11-08 22:25:46 +0000

    delete file

--- Task Manager/ViewController.swift
+++ Task Manager/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("My name is Kaj")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
